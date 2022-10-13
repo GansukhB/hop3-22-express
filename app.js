d1548364e1ee4604584c54d2c1bde689f506d715
Gansukh Batjargal <gansukhb@Gansukhs-MacBook-Pro.local>
2022-10-13 18:43:23 +0800

    post put methods

--- app.js
+++ app.js
@@ -13,9 +13,22 @@ app.get("/greet/:name", (req, res) => {
   res.send("Hello, " + req.params.name);
 });
 
-app.put("/user", (req, res) => {
-  console.log(req.body);
+app.post("/user", (req, res) => {
+  users.push(req.body);
+  console.log(users);
   res.send("user created");
+});
+
+app.put("/user", (req, res) => {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].email === req.body.email) {
+      users[i].name = req.body.name;
+      users[i].password = req.body.password;
+      break;
+    }
+  }
+  console.log(users);
+  res.send("user not found");
 });
 
 app.listen(3000, () => {
